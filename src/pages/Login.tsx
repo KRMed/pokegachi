4b29b2b779751d3a39ca987a0576c53ce9221354
ysabelleablir <ysabelleablir@gmail.com>
2026-07-06 14:57:41 -0400

    base login design

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,27 @@
 import './login_register.css';
+import logo from '/public/home.png'
 
 export default function Login() {
-  return <h1>Login time</h1>;
+
+  return (
+    <div className="login-page">
+        <div className="focus">
+            <img className="logo" src={logo} />
+            <form className="panel">
+                <label>
+                    Username
+                    <input type="text" />
+                </label>
+                <label>
+                    Password
+                    <input type="password" />
+                </label>
+
+                <button type="submit">
+                    Login
+                </button>
+            </form>
+        </div>
+    </div>
+  );
 }
