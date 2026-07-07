c4998a8463d637bac59d4a1ee0d8a404f5b3664d
Kristofer Medeiros <kristoferavka@gmail.com>
2026-07-07 15:21:15 -0400

    Added pokemon api and first roll

---
+++ src/lib/pokeapi.ts
@@ -0,0 +1,15 @@
+let list: { name: string }[] = [];
+
+export async function getPokemonList() {
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
+  const data = await response.json();
+  list = data.results;
+}
+
+export function getRandomPokemon() {
+  const randomIndex = Math.floor(Math.random() * list.length);
+  return {
+    name: list[randomIndex].name,
+    sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${randomIndex + 1}.png`
+  };
+}
--- src/pages/First.tsx
+++ src/pages/First.tsx
@@ -1,12 +1,26 @@
 import Button from '../components/button';
 import './first.css';
+import { useState, useEffect } from 'react';
+import { getPokemonList, getRandomPokemon } from '../lib/pokeapi.ts';
 
 export default function First() {
+    const [pokemon, setPokemon] = useState<{ name: string; sprite: string } | null>(null);
+
+    useEffect(() => {
+        getPokemonList();
+    }, []);
+
     return (
     <div className="page">
       <div className="stuff">
-        <h1 className="text-box">It's time to roll for your first Pokemon!</h1>
-        <Button text="Roll" />
+        <h1 className="text-box">
+            {pokemon ? `You got ${pokemon.name}!` : "It's time to roll for your first Pokemon!"}
+        </h1>
+        <img 
+            className="pokemon-img"
+            src={pokemon ? pokemon.sprite : "/mystery.png"} 
+        />
+        <Button text="Roll" onClick={() => setPokemon(getRandomPokemon())} />
       </div>
     </div>
   );
