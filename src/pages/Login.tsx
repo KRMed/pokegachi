c1ea7fa204d56ffbda4887cdd21512f1417b75ca
Aidan Jiang <171952810+ajiang05@users.noreply.github.com>
2026-07-07 14:55:18 -0400

    Added supabase client, funcitonality for login and register

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,27 +1,60 @@
-import './login_register.css';
-import logo from '/pokegachi_logo.png'
+import "./login_register.css";
+import logo from "/pokegachi_logo.png";
+import { useEffect, useState } from "react";
+import { supabase } from "../lib/supabase";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    supabase.auth.getSession().then((result) => {
+      console.log(result);
+    });
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    supabase.auth.signInWithPassword({ email, password }).then((result) => {
+      console.log(result);
+
+      if (result.error) {
+        setErrorMessage(result.error.message);
+      } else {
+        setErrorMessage("");
+      }
+    });
+  };
+
+  console.log(email, password);
 
   return (
     <div className="login-page">
-        <div className="login-focus">
-            <img className="logo" src={logo} />
-            <form className="panel">
-                <label>
-                    Username
-                    <input type="text" />
-                </label>
-                <label>
-                    Password
-                    <input type="password" />
-                </label>
-
-                <button type="submit">
-                    Login
-                </button>
-            </form>
-        </div>
+      <div className="login-focus">
+        <img className="logo" src={logo} />
+        <form className="panel" onSubmit={handleSubmit}>
+          <label>
+            Email
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+          <label>
+            Password
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+          {errorMessage && <p className="error-message">{errorMessage}</p>}
+          <button type="submit">Login</button>
+        </form>
+      </div>
     </div>
   );
 }
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,26 +1,51 @@
-import './login_register.css';
-import logo from '/pokegachi_logo.png'
+import "./login_register.css";
+import logo from "/pokegachi_logo.png";
+import { useEffect, useState } from "react";
+import { supabase } from "../lib/supabase";
 
 export default function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    supabase.auth.getSession().then((result) => {
+      console.log(result);
+    });
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    supabase.auth.signUp({ email, password }).then((result) => {
+      console.log(result);
+    });
+  };
+
   return (
     <div className="login-page">
-        <div className="focus">
-            <img className="logo" src={logo} />
-            <form className="panel">
-                <label>
-                    Username
-                    <input type="text" />
-                </label>
-                <label>
-                    Password
-                    <input type="password" />
-                </label>
+      <div className="focus">
+        <img className="logo" src={logo} />
+        <form className="panel" onSubmit={handleSubmit}>
+          <label>
+            Email
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+          <label>
+            Password
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
 
-                <button type="submit">
-                    Create Account
-                </button>
-            </form>
-        </div>
+          <button type="submit">Create Account</button>
+        </form>
+      </div>
     </div>
   );
 }
